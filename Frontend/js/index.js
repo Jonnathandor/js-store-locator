38582d1ca870c1dcd10d19aab109689d68799cf3
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-06-13 21:16:32 -0500

    added header inputs and style

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -7,6 +7,6 @@ function initMap() {
     // Create the map.
     const map = new google.maps.Map(document.getElementById('map'), {
         zoom: 7,
-        center: { lat: 52.632469, lng: -1.689423 },
+        center: { lat: 34.063380, lng: -118.358080 },
     });
 }
