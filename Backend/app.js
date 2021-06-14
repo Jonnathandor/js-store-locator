75f317cd005594d6eed4e4039d0c12e0fae7e04f
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-06-14 18:25:36 -0500

    added stores post endpoint

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json({ limit: '50mb'}));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
+})
+
+app.post('/api/stores', (req, res) => {
+    let db = req.body;
+    console.log(db)
+    res.send(db);
 })
 
 app.listen(port, () => {
