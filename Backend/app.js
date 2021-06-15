0d51b16024aab95a79f2507e1dcd4fc237b73ce8
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-06-14 19:30:15 -0500

    Connected the db

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config()
 const app = express();
 const port = 3000;
+const Store = require('./Models/Store');
+
+mongoose.connect(`mongodb+srv://jonnathan_store_locator:${process.env.DB_PASSWORD}@cluster0.e1ygu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 app.use(express.json({ limit: '50mb'}));
 
@@ -11,7 +19,25 @@ app.get('/', (req, res) => {
 app.post('/api/stores', (req, res) => {
     let db = req.body;
     console.log(db)
-    res.send(db);
+    let stores = new Store({
+        storeName: 'Test2',
+        phoneNumber: '133434534',
+        location: {
+            type: 'Point',
+            coordinates: [
+                -118.376354,
+                34.063584
+            ]
+        }
+    })
+    stores.save();
+    res.send(stores);
+})
+
+app.delete('/api/stores', (req, res) => {
+    Store.deleteMany({}, (err) => {
+        res.status(200).send(err)
+    })
 })
 
 app.listen(port, () => {
